c4a603cf7a780267e80c1bc6edfa7e92754cdf47
Kalyani  Mashiwal <kalyani.masiwal@gmail.com>
2016-12-16 10:48:29 +0530

    Update DictionariesandMaps.java

--- DictionariesandMaps.java
+++ DictionariesandMaps.java
@@ -1,3 +1,12 @@
+/**
+Given n  names and phone numbers, assemble a phone book that maps friends' names 
+to their respective phone numbers. You will then be given an unknown number
+of names to query your phone book for. For each name  queried, print
+the associated entry from your phone book on a new line in the form name=phoneNumber;
+if an entry for name  is not found, print Not found instead.
+**/
+
+
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -26,8 +35,8 @@ public class DictionariesandMaps {
 			} else {
 				list.add(s + "=" + data.get(s));
 			}
-			for (String s1 : list) {
-				System.out.println(s1);
+			for (String finalResult : list) {
+				System.out.println(finalResult);
 			}
 		}
 
